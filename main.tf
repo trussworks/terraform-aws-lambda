c52239c7fc06a9b1f95d58ea1b62abd1f3afc560
Chikara Takahashi <chtakahashi10@gmail.com>
2023-08-04 12:10:32 -0400

    only sourceArn as the condition

--- main.tf
+++ main.tf
@@ -22,11 +22,6 @@ data "aws_iam_policy_document" "assume_role" {
       identifiers = ["lambda.amazonaws.com"]
     }
     condition {
-      test     = "StringEquals"
-      variable = "aws:SourceAccount"
-      values   = [data.aws_caller_identity.current.account_id]
-    }
-    condition {
       test     = "ArnLike"
       variable = "aws:SourceArn"
       values   = ["arn:${data.aws_partition.current.partition}:lambda:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:function:${local.full_name}"]
